623bdd9904ea17bf7054517cb629ae0e00e2e1d3
TiaraILogan <34221752+tiaralogan@users.noreply.github.com>
2023-04-22 14:42:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
+
+//module.exports = nextConfig;
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  basePath: '/',
+  assetPrefix: '/',
   trailingSlash: true,
 };
 

d9d68597401b4ad3ec4dfa1d408f03cc5ff3c297
TiaraILogan <34221752+tiaralogan@users.noreply.github.com>
2023-04-22 14:46:35 -0400

    Update constants.tsx

--- components/constants.tsx
+++ components/constants.tsx
@@ -16,9 +16,11 @@ export const SOCIAL_MEDIA = [
     { id: "buymecoffee", icon: <BiCoffeeTogo />, link: "https://www.buymeacoffee.com/tiaraloganX" },
     { id: "linkedin", icon: <FaLinkedin />, link: "https://www.linkedin.com/in/tiara-logan/" },
     { id: "github", icon: <FaGithub />, link: "https://github.com/tiaralogan" },
-    {/*{ id: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/thejereface/" }
-    { id: "spotify", icon: <FaSpotify />, link: "https://open.spotify.com/user/12130662157?si=df6e90755dd44083" },*/}
 
 ];
 
 export const COPYRIGHT = "2023 tiaralogan.com. All Rights Reserved.";
+
+
+{/*{ id: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/thejereface/" }
+    { id: "spotify", icon: <FaSpotify />, link: "https://open.spotify.com/user/12130662157?si=df6e90755dd44083" },*/}
